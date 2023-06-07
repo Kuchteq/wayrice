fca1f5152bd9597cf6fc1f9eff270b4a5431f5eb
kuchtan <mariusz.kuchta@triona.fi>
2023-06-07 15:00:37 +0300

    Add some initial java settings

---
+++ .config/nvim/lua/plugins/java.lua
@@ -0,0 +1,11 @@
+return {
+	"mfussenegger/nvim-jdtls",
+	ft = "java",
+	config = function()
+		local config = {
+			cmd = { '/path/to/jdt-language-server/bin/jdtls' },
+			root_dir = vim.fs.dirname(vim.fs.find({ 'gradlew', '.git', 'mvnw' }, { upward = true })[1]),
+		}
+		require('jdtls').start_or_attach(config)
+	end
+}
