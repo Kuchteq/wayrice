757b12de494d54928ad12fefff73eb92152eb2ec
maniu <maniuchta3@gmail.com>
2023-06-23 13:09:07 +0300

    A small rewrite of dap mode

--- .config/nvim/lua/plugins/daprc.lua
+++ .config/nvim/lua/plugins/daprc.lua
@@ -4,15 +4,25 @@ DEBUGGABLE_MODE_STATUS = false
 DEBUG_MODE_BUFFER_NR = -100
 DEBUG_MODE_AUTOCMD_ID = -10
 
-local function toggleEasyDebug()
+local M = {};
+
+P = function(table) print(vim.inspect(table)); end
+
+M._styleNamespace = nil;
+M._previousDebugCursorRow = nil;
+M._followCursorAutocmdId = nil;
+M._styleNamespace = nil;
+
+M.toggleEasyDebug = function()
 	-- automatically check if there's a launch.json available and add configuration present there
 	require('dap.ext.vscode').load_launchjs(".vscode/launch.json");
 	local stackmap = require("stackmap")
 	local lualine = require("lualine")
+	M._styleNamespace = vim.api.nvim_create_namespace("currentCursorNs");
 	if DEBUGGABLE_MODE_STATUS == false then
 		lualine.setup({ sections = { lualine_b = { { function() return "EASY DEBUG ON" end, color = { fg = "#e57474", gui = "bold" } }, "diagnostics" } } })
 		stackmap.push("debuggable", "n", {
-			{ "q", debug_mode_toggle, desc = "debuggable" }
+			{ "q", M.debug_mode_toggle, desc = "debuggable" }
 		})
 	else
 		stackmap.pop("debuggable", "n")
@@ -21,19 +31,48 @@ local function toggleEasyDebug()
 	DEBUGGABLE_MODE_STATUS = not DEBUGGABLE_MODE_STATUS
 end
 
--- require'dap.breakpoints'.set({},vim.api.nvim_buf_get_number(0), 22) set stuff programatically
-function debug_mode_toggle()
-	local stackmap = require("stackmap")
+M._modeChangeApperance = function(toDebug)
 	local lualine = require("lualine")
+	local palette = require('everblush.palette')
+	if toDebug then
+		lualine.setup({ sections = { lualine_a = { { function() return "DEBUG" end, color = { bg = "#e57474", fg = "#000000" } } } } })
+		local lineBg = vim.api.nvim_get_hl(0, { name = "CursorLine" }).bg;
+		vim.api.nvim_set_hl(M._styleNamespace, 'CursorLine', {  fg = palette.color4, bg = lineBg,   bold = true })
+		vim.api.nvim_set_hl(M._styleNamespace, 'CursorLineNr', { fg = palette.color4, bg = lineBg  })
+		vim.api.nvim_set_hl_ns(M._styleNamespace);
+
+		M._previousDebugCursorRow = vim.api.nvim_win_get_cursor(0)[1]
+		vim.fn.sign_place(420, "currentCursorNs", "CurrentCursorDebugLine",1, {lnum=M._previousDebugCursorRow });
+
+		M._followCursorAutocmdId = vim.api.nvim_create_autocmd("CursorMoved", {
+			callback = function()
+				local currentRow = vim.api.nvim_win_get_cursor(0)[1]
+				if M._previousDebugCursorRow ~= currentRow then
+					vim.fn.sign_unplace("currentCursorNs", {buffer=1, id=420});
+					vim.fn.sign_place(420, "currentCursorNs", "CurrentCursorDebugLine",1, {lnum=currentRow});
+				end
+				M._previousDebugCursorRow = currentRow;
+			end,
+		})
+	else
+		vim.api.nvim_set_hl_ns(0)
+		vim.api.nvim_del_autocmd(M._followCursorAutocmdId);
+		vim.fn.sign_unplace("currentCursorNs", {buffer=1, id=420});
+		lualine.setup({ sections = { lualine_a = { "mode" } } })
+	end
+end
+
+
+-- require'dap.breakpoints'.set({},vim.api.nvim_buf_get_number(0), 22) set stuff programatically
+M.debug_mode_toggle = function()
+	local stackmap = require("stackmap")
 	local widgets = require('dap.ui.widgets')
 	local varsidebar = widgets.sidebar(widgets.scopes, { width = 50 })
-	local palette = require('everblush.palette')
 	if DEBUG_MODE_STATUS == false then
 		stackmap.pop("debuggable", "n");
 		local bufnr = vim.api.nvim_get_current_buf()
 		DEBUG_MODE_BUFFER_NR = bufnr
 		stackmap.push("debug_mode", "n", {
-			--{ "<leader>dB", function() require("dap").set_breakpoint(vim.fn.input('Breakpoint condition: ')) end, desc = "Breakpoint Condition" },
 			{
 				"<F2>",
 				function()
@@ -61,41 +100,30 @@ function debug_mode_toggle()
 				{},
 				desc = "Toggle Breakpoint"
 			},
+			{ "<esc>", M.debug_mode_toggle,                               desc = "Escape debug" },
 			{ "A",     function() require("dap").clear_breakpoints() end, {},                               desc = "Clear all breakpoitns" },
 			{ "c",     function() require("dap").continue() end,          { silent = true, nowait = true }, desc = "Continue" },
 			{ "o",     function() require("dap").run_to_cursor() end,     {},                               desc = "Run to Cursor" },
-			{ "<esc>", debug_mode_toggle,                                 desc = "Escape debug" },
-			--{ "dg", function() require("dap").goto_() end, desc = "Go to line (no execute)" },
-			--{ "di", function() require("dap").step_into() end, desc = "Step Into" },
 			{ "n",     function() require("dap").down() end,              {},                               desc = "Down" },
 			{ "p",     function() require("dap").up() end,                {},                               desc = "Up" },
 			{ "r",     function() require("dap").run_last() end,          {},                               desc = "Run Last" },
 			{ "q",     function() require("dap").terminate() end,         {},                               desc = "Run Last" },
-
-			--{ "do", function() require("dap").step_out() end, desc = "Step Out" },
-			-- D like down, down we go further
 			{ "d",     function() require("dap").step_over() end,         { silent = true, nowait = true }, desc = "Step Over" },
-			--{ "dp", function() require("dap").pause() end, desc = "Pause" },
 			{ "<c-r>", function() require("dap").repl.toggle() end,       { nowait = true },                desc = "Toggle REPL" },
 			{ "<c-v>", function() varsidebar.toggle() end, { nowait = true }, desc = "Toggle Variables"
 			},
-			--{ "ds", function() require("dap").session() end, desc = "Session" },
 			{ "u", function() require("dap").step_back() end,        {},                desc = "Step Back" },
-			--{ "a",     function() require("dapui").eval() end,            { , nowait = true },                desc = "Eval" },
 			{ "a", function() require("dap.ui.widgets").hover() end, { nowait = true }, desc = "Eval" },
 		})
 		stackmap.push("with_ui", "v", {
 			{ "a", function() require("dapui").eval() end, { nowait = true }, desc = "Eval" },
 		})
-		lualine.setup({ sections = { lualine_a = { { function() return "DEBUG" end, color = { bg = "#e57474", fg = "#000000" } } } } })
-		vim.api.nvim_set_hl(0, 'CursorLine', { ctermbg = 0, fg = palette.color4, bg = '#263252' })
+		M._modeChangeApperance(true);
 	else
+		M._modeChangeApperance(false);
 		stackmap.pop("debug_mode", "n")
 		stackmap.pop("with_ui", "n")
-		stackmap.push("debuggable", "n", { { "q", debug_mode_toggle, desc = "debuggable" } })
-
-		vim.api.nvim_set_hl(0, 'CursorLine', { ctermbg = 10, bg = palette.cursorline })
-		lualine.setup({ sections = { lualine_a = { "mode" } } })
+		stackmap.push("debuggable", "n", { { "q", M.debug_mode_toggle, desc = "debuggable" } })
 	end
 	DEBUG_MODE_STATUS = not DEBUG_MODE_STATUS
 end
@@ -111,6 +139,7 @@ return {
 			cmd = { "DapInstall", "DapUninstall" },
 			config = function()
 				local palette = require('everblush.palette')
+
 				vim.api.nvim_create_autocmd({ "FileType" }, {
 					pattern = "dap-float",
 					callback = function(ev)
@@ -126,6 +155,7 @@ return {
 				vim.fn.sign_define('DapLogPoint', { text = '', texthl = 'DapLogPoint', linehl = 'DapLogPoint', numhl = 'DapLogPoint' })
 				vim.fn.sign_define('DapStopped', { text = '', texthl = 'DapStopped', linehl = 'DapStopped', numhl = 'DapStopped' })
 
+				vim.fn.sign_define('CurrentCursorDebugLine', { text = '>>', texthl = 'CursorLine', linehl = 'CursorLine', numhl = 'CursorLineNr' })
 				require("nvim-dap-virtual-text").setup()
 				require("mason-nvim-dap").setup({
 					ensure_installed = { "python" },
@@ -172,6 +202,6 @@ return {
 	keys = {
 		{ "<leader>d", function()
 			--require("dapui").open()
-			toggleEasyDebug()
+			M.toggleEasyDebug()
 		end } },
 }
